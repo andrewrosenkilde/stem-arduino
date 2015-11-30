80ab6e8f2fe4253f24b18abd9ccf6a466e903306
Andrew Rosenkilde <andrewrosenkilde@users.noreply.github.com>
2015-11-30 13:20:47 -0500

    minified whitespace

--- F_forcecalibration.ino
+++ F_forcecalibration.ino
@@ -2,13 +2,9 @@ void setup() {
     Serial.begin(9600);
     pinMode(8, INPUT);
 }
-
 void loop() {
-    
     int val = digitalRead(8);
-    
     if (val == HIGH) {
-      
         Serial.println("Taking force data...");
         int total = 0;
         for(int count = 0; count<=9; count++) {
@@ -16,10 +12,8 @@ void loop() {
             total = total + analogRead(0);
             delay(100);
         }
-        
         int averageForce = total/10;
         Serial.println("The average force value is " + String(averageForce)) + ".";
-    
     }
     delay(200);
 }
